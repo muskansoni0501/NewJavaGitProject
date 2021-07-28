97420859ca195159702e4a820b84444d587b9881
muskansoni0501 <muskansoni0501@gmail.com>
2021-07-28 15:25:09 +0530

    Sixth Commit

--- NewJavaGitProject/src/main/java/com/lti/demo/MyFile.java
+++ NewJavaGitProject/src/main/java/com/lti/demo/MyFile.java
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 package com.lti.demo;
 
-=======
->>>>>>> branch 'main' of https://github.com/muskansoni0501/NewJavaGitProject.git
 public class MyFile {
  public static void main(String args[]){
-<<<<<<< HEAD
 	 
    System.out.println("Hello Git");
    
-=======
-   System.out.println("Hello Git");
->>>>>>> branch 'main' of https://github.com/muskansoni0501/NewJavaGitProject.git
  }
  
 }
